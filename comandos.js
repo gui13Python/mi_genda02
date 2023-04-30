c2655ed52c6015530248039e8d11c47adaa43bc0
leandro costa <91476305+gui13Python@users.noreply.github.com>
2023-04-30 09:54:53 -0300

    up js
    up js

--- comandos.js
+++ comandos.js
@@ -1,8 +1,35 @@
-function inicia(){
-    document.getElementById("").addEventListener("click", function(){
-        adicionar();
+
+function add(){
+
+let novo_elemento = document.createElement('textarea')
+novo_elemento.className = "card-childen";
+let  elemento = document.querySelector(".card-childen")
+elemento.appendChild(novo_elemento  );
+
+}
+
+add()
+
+
+
+
+function dele(){
+let remove_tarefa = document.querySelector('textarea ');
+remove_tarefa.remove("novo_elemento  " );
+
     
-    
-});
+
+}
+
+
+
+
+dele()
+
+
+
+function crea(){
+let pega = document.querySelector(".card-childen");
+
 
 }
